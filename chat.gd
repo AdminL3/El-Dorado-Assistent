e6d65436cf74226156909f6a3ab8cdaf2de339ac
Levi <l-blu@outlook.de>
2024-06-02 12:54:42 +0200

    Tried some fixing for multiplayer

--- chat.gd
+++ chat.gd
@@ -12,22 +12,29 @@ var msg : String
 
 
 func _on_host_pressed():
-	var peer =ENetMultiplayerPeer.new()
+	var peer = ENetMultiplayerPeer.new()
 	peer.create_server(1477)
 	get_tree().set_multiplayer(SceneMultiplayer.new(),self.get_path())
 	multiplayer.multiplayer_peer = peer
 	joined()
 
 func _on_join_pressed():
-	var peer =ENetMultiplayerPeer.new()
+	var peer = ENetMultiplayerPeer.new()
 	peer.create_client("79.199.160.21",1477)
+	print("Attempting to connect to server...")
 	get_tree().set_multiplayer(SceneMultiplayer.new(),self.get_path())
 	multiplayer.multiplayer_peer = peer
-	joined()	
+	if peer.get_connection_status() == ENetMultiplayerPeer.CONNECTION_CONNECTED:
+		print("Connection successful")
+	else:
+		print("Connection failed")
+	joined()
+	
 
 func _on_send_pressed():
 	if multiplayer.multiplayer_peer.get_connection_status() == ENetMultiplayerPeer.CONNECTION_CONNECTED:
 		rpc("msg_rpc", usrn , message.text)
+		print("sent")
 	else:
 		print("Peer is not connected")
 
