6c9137543560f04aee427bb95727516766222f10
Levi <l-blu@outlook.de>
2024-06-12 18:03:15 +0200

    stuff

--- scripts/Area2D.gd
+++ scripts/Area2D.gd
@@ -1,15 +1,11 @@
 extends Area2D
 
 @export var index: int = -1
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	pass 
+	
 func _process(delta):
 	pass
-
 
 func _on_mouse_entered():
 	pass # Replace with function body.
